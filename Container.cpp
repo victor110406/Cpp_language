75540b195641b6ebec206c48cc0f529ca5768adf
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-06-04 21:39:47 +0900

    Add files via upload

---
+++ Container.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//컨테이너는 같은 타입의 여러 객체를 저장하는 일종의 집합이다.
+//컨테이너는 클래스 템플릿으로, 컨테이너 변수를 선언할 때 컨테이너에 포함할 요소의 타입을 명시가능
+//컨테이너는 복사 생성과 대입으 할 수 있는 타입의 객체만을 저장가능
+//컨테이너는 요소의 추가 및 제거를 포함한 다양한 작업을 도와주는 멤버 함수를 포함
+
+//1. 시퀀스 컨테이너(sequence container)
+//2. 연관 컨테이너(associative container)
+//3. 컨테이너 어댑터(adapter container)
+
+int main() {
+
+	return 0;
+}
